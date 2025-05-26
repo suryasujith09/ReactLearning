dd26635197a6aa08b7867245b0f344b672c3535b
Metta Surya Sujith <mettasuryasujith09@gmail.com>
2025-05-26 09:41:04 +0000

    myturn 1 surya

--- effect-hook-part-2/src/App.jsx
+++ effect-hook-part-2/src/App.jsx
@@ -5,3 +5,9 @@ import './App.css'
 const App = () => <BmiCalculator />
 export default App
 // Hello Hi I am Pranay
+// import React from 'react'
+// import './App.css'
+// import BmiCalculator from './components/BmiCalculator'
+//
+// const App = () => {
+//   return
