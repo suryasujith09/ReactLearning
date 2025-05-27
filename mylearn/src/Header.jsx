32760d5374c8b750f9142cec21b915650ad7860c
Metta Surya Sujith <mettasuryasujith09@gmail.com>
2025-05-27 03:46:23 +0000

    card page is also added in mylearn

--- mylearn/src/Header.jsx
+++ mylearn/src/Header.jsx
@@ -1,9 +1,17 @@
-function Header() {
+import React from 'react'
+import Photo from '/src/assets/photo.jpg'
+
+const ProfileCard = () => {
   return (
-    <header>
-      <h1>My Website</h1>
-    </header>
+    <div className="card">
+      <div className="imgc">
+        <img src={Photo} alt="Profile" className="photo" />
+      </div>
+      <h2 className="name">METTA SURYA SUJITH</h2>
+      <p className="idp">ID: N24H01A0390</p>
+      <p className="php">Ph no: +91-8885492229</p>
+    </div>
   )
 }
 
-export default Header
+export default ProfileCard
