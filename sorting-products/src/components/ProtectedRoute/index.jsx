af5d43236b4d9733c3c4a507ec325ae230f07442
Metta Surya Sujith <mettasuryasujith09@gmail.com>
2025-06-10 10:20:28 +0000

    heloo

--- sorting-products/src/components/ProtectedRoute/index.jsx
+++ sorting-products/src/components/ProtectedRoute/index.jsx
@@ -13,3 +13,5 @@ export default ProtectedRoute
 
 //hello thius i s protected route
 // this is a protected route component that checks for a JWT token in cookies
+// here, if the token is not present, it redirects the user to the login page
+// if the token is present, it renders the children components
